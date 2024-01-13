2975baaa24042f40f166d20dd68005a013c32f47
bitlover2009 <bitlover2009@gmail.com>
2024-01-12 18:18:55 -0600

    Initial Commit

---
+++ Scanner/ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerApp.swift
+//  Scanner
+//
+//  Created by David Grau Beltrán  on 12/01/24.
+//
+
+import SwiftUI
+
+@main
+struct ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
